f61fc9288fa7ed2240b18531ac0b24bf59981503
Rafael Deroncio <rafael.deroncio@animaeducacao.com.br>
2023-04-28 11:36:16 -0300

    add service cliente T

--- Menu_forms/WindowsFormsUPSKILLINGGAMA/Services/ClienteService.cs
+++ Menu_forms/WindowsFormsUPSKILLINGGAMA/Services/ClienteService.cs
@@ -3,10 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WindowsFormsUPSKILLINGGAMA.Models;
+using WindowsFormsUPSKILLINGGAMA.Services.Interfaces;
 
 namespace WindowsFormsUPSKILLINGGAMA.Services
 {
-    internal class Class1
+    public class ClienteService<T> : IBaseService<T> where T : ClienteModel
     {
+        public bool Alterar(T entity)
+        {
+            // _persistencia.Alterar(entity);
+            throw new NotImplementedException();
+        }
+
+        public bool Cadastrar(T entity)
+        {
+            entity.Id = 0; //_persistencia.GetNewId();
+            // _persistencia.Cadastrar(entity);
+            throw new NotImplementedException();
+        }
+
+        public bool Excluir(T entity)
+        {
+            // _persistencia.Excluir(entity);
+            throw new NotImplementedException();
+        }
+
+        public bool Recuperar(T entity)
+        {
+            // _persistencia.Recuperar(entity);
+            throw new NotImplementedException();
+        }
     }
 }
